71b9ab54a5a29e5f43dff23ef9e988cf5ebd0d9e
sundeeep <sundeeepdev@gmail.com>
2026-01-30 16:53:44 +0530

    cors, cookie-parser middlewares used

--- express-js/learning-backend/src/index.js
+++ express-js/learning-backend/src/index.js
@@ -3,10 +3,39 @@ import demoRouter from "./routes/demoRoute.js";
 import connectMongoDb from "./config/mongo-db.js";
 import userRouter from "./routes/userRoute.js";
 import bookRouter from "./routes/bookRoute.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
-
 app.use(express.json());
+app.use(cookieParser());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://sundeeep.vercel.app"
+];
+
+// CORS options
+ const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.post("/testing", (req, res) => {
+    res.status(200).json({
+        message: "/testing route has been hit successfully!",
+        data: req.body,
+        cookies: req.cookies
+    })
+})
 
 app.use("/demo", demoRouter)
 app.use("/users", userRouter)
--- reactjs/reactjs-fiber/src/App.jsx
+++ reactjs/reactjs-fiber/src/App.jsx
@@ -1,11 +1,37 @@
-import TimerApp from "./components/TimerApp.jsx";
-
 
 function App(){
 
+  const sendHTTPRequest = async() => {
+    try {
+      const URL = "http://localhost:8000/testing";
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          name: "Sundeeep Dasari",
+          age: 25
+        }),
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+
+      const response = await fetch(URL, options);
+      console.log(response)
+      if(!response.ok) {
+        throw new Error("Invalid API Endpoint!")
+      }
+      const jsonData = await response.json();
+      console.log(jsonData)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
   return (
     <>
-    <TimerApp />
+    <button onClick={sendHTTPRequest}>Send Request</button>
     </>
   )
 }
